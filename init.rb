bc68cd7a59bb7529868adc211499c79e0d121f20
RedminePower <redmine.power.com@gmail.com>
2022-11-26 21:25:29 +0900

    バージョンの変更忘れ

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_teams_button do
     author_url 'https://www.en.redmine-power.com/'
     description 'Add "Teams" button to the username of the issue page.'
     url 'https://github.com/RedminePower/redmine_teams_button'
-    version '1.0.0'
+    version '1.1.0'
 
     settings default: {'empty' => true}, partial: 'settings/redmine_teams_button'
 
@@ -16,5 +16,3 @@ Redmine::Plugin.register :redmine_teams_button do
         permission :teams_button, :teams_button => [:index]
     end
 end
-
-Rails.logger.info 'Finishing Redmine Teams Button'
